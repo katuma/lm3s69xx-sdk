1878b2799d846b6f676da2585ff53a34202f836e
Karel Tuma <karel.tuma@gmail.com>
2013-01-11 04:54:22 +0100

    Fix tun in test script.
    Signed-off-by: Karel Tuma <karel.tuma@gmail.com>

--- test.sh
+++ test.sh
@@ -3,6 +3,9 @@ if ( echo -e "test123\nexit"  | qemu-system-arm -nographic -M lm3s6965evb --kern
 	echo -e "\e[1;32mSerial passed.\e[0m"
 fi
 
+modprobe tun
+tunctl -t tap0
+ifconfig tap0 172.32.0.1 netmask 255.255.255.0
 dnsmasq -d --interface=tap0 --dhcp-range=172.32.0.2,172.32.0.2 &
 DPID=$?
 qemu-system-arm -nographic -M lm3s6965evb --kernel ex2 -net nic -net tap,ifname=tap0,script=no < /dev/null &
