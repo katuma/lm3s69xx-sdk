8a3bdb1335d3e4ebd18092607f03eedfcb90925d
Karel Tuma <karel.tuma@gmail.com>
2013-01-23 15:47:06 +0100

    Wait a bit longer for dhcp

--- test.sh
+++ test.sh
@@ -11,8 +11,7 @@ DPID=$?
 qemu-system-arm -nographic -M lm3s6965evb --kernel ex2 -net nic -net tap,ifname=tap0,script=no < /dev/null &
 QPID=$?
 
-if ((echo -e "test123"; sleep 3; echo "exit") | nc 172.32.0.2 9000) | grep 321tset; then
-	echo "testing"
+if ((echo -e "test123"; sleep 10; echo "exit") | nc 172.32.0.2 9000) | grep 321tset; then
 	echo -e "\e[1;32mNetworking passed.\e[0m"
 fi
 kill -KILL $DPID $QPID
